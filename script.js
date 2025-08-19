e297c96a59e35adb552cb702553a9b290b921ab6
aveencoder <aveen598@gmail.com>
2025-08-19 17:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function indexOfIgnoreCase(s1, s2) {
-  return s1.lowerCase().indexOf(s2.lowerCase());
+ return (s1.toLowerCase()).indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
